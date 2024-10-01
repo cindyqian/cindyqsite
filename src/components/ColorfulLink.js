c80c24d8c419a8941010c8344c16e6b8bebd7877
Cindy Qian <cindyqian.yushi@gmail.com>
2024-10-01 14:11:29 -0700

    Updates

--- src/components/ColorfulLink.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'; //import React library
-
-
-export function ColorfulLink(props) {
-    return (
-        <a href={props.link} target="_blank" className="font-medium text-yellow-500 hover:text-yellow-600">{props.text}</a>
-    );
-}
